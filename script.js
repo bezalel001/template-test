e262dcd73e1be9f6f70f39af41c283c57c82118c
bezalel001 <mickeybezalel@gmail.com>
2020-08-01 12:20:53 +0100

    added loader

--- script.js
+++ script.js
@@ -3,8 +3,25 @@ const quoteText = document.getElementById('quote-text');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
+let counter = 0;
+
+const showLoadingSpinner = () => {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+};
+
+const removeLoadingSpinner = () => {
+	if (!loading.hidden) {
+		quoteContainer.hidden = false;
+		loader.hidden = true;
+	}
+};
+
+// Get Quote From API
 const getQuote = async () => {
+	showLoadingSpinner();
 	const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 	const apiUrl =
 		'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
@@ -22,8 +39,15 @@ const getQuote = async () => {
 		}
 
 		quoteText.innerText = data.quoteText;
+		removeLoadingSpinner();
 	} catch (error) {
-		getQuote();
+		if (counter++ < 20) {
+			getQuote();
+		} else {
+			counter = 0;
+			alert('Could load quotes! Please refresh page');
+			return;
+		}
 	}
 };
 
@@ -41,3 +65,4 @@ twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuote();
+// loading();
